3697ddccb717d0b46dd943bf0d5e9f0a03905514
oliverdudas <oliverdudas@gmail.com>
2015-01-31 16:27:00 +0100

    Initial commit

---
+++ android/src/com/dudas/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dudas.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dudas.game.MultipleStagesExample;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MultipleStagesExample(), config);
+	}
+}
---
+++ desktop/src/com/dudas/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dudas.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dudas.game.MultipleStagesExample;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MultipleStagesExample(), config);
+	}
+}
